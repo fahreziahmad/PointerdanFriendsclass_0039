791ec1cebd3330c91bcd124674f539cd25cd64f9
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-05-07 17:55:22 +0700

    Add project files.

---
+++ PemDas1/PemDas1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+	public:
+		int nim;
+		void showNim() {
+			cout << "No Induk = " << nim << endl;
+		}
+};
+
+int main() {
+	mahasiswa mhs{ 1 };//objek mhs
+	mhs.showNim();//
+
+	mahasiswa &refMhs = mhs; //
+	refMhs.nim = 2;//
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //
+	pMhs->nim = 3; //
+	pMhs->showNim();
+	return 0;
+}
